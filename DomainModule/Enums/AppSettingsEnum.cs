85e1eea589767152c26032331f438aecdb809d2d
common khadka <commonkhadka@gmail.com>
2023-12-02 08:10:41 +0545

    Add Pin to appsetting

--- DomainModule/Enums/AppSettingsEnum.cs
+++ DomainModule/Enums/AppSettingsEnum.cs
@@ -10,6 +10,8 @@ namespace DomainModule.Enums
     public enum AppSettingsEnum
     {
         [Display(Name = "Encryption Key")]
-        EncryptionKey
+        EncryptionKey,
+        [Display(Name = "Pin")]
+        ExportPin
     }
 }
